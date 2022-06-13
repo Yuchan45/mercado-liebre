9c48f0e85815082076c48c10aacc93683591e318
Yuchan <yu.nakasone@gmail.com>
2022-06-13 09:27:51 -0300

    nodemon app.js -> node app.js' para que lo admita Heroku

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const app = express();
 
+//MIDDLEWARE
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+app.set('puerto', process.env.PORT || 3001);
 
+// **** RUTASS ****
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
@@ -18,6 +21,10 @@ app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.listen(3000, () => {
-    console.log("Sevidor corriendo en puerto 3000");
+// app.listen(3000, () => {
+//     console.log("Sevidor corriendo en puerto 3000");
+// });
+
+app.listen(app.get('puerto'), () => {
+    console.log(`Servidor corriendo de manera satisfactoria ${app.get('puerto')}`);
 });
